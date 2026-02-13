dedd8d63893a9e3a44b8477407b177ecb1acd67d
cscaff <cscaff@ufl.edu>
2026-02-12 21:38:42 -0500

    changed button

--- lab_1/lab1.sv
+++ lab_1/lab1.sv
@@ -135,14 +135,16 @@ module lab1( input logic        CLOCK_50,  // 50 MHz Clock input
                         FINISH: begin
                               if (c0 && !c1 && !c2 && !c3) begin
                                     // Bound offset to 0-255
-                                    if (offset != 8'hFF)
+                                    if (offset != 8'hFF) begin
                                           offset <= offset + 12'd1;
-                                          start <= {24'b0, offset[7:0]};
+                                          start <= {24'b0, offset[7:0] + 8'd1};
+                                    end
                               end else if (!c0 && c1 && !c2 && !c3) begin
                                     // Bound offset to 0-255
-                                    if (offset != 8'h00)
+                                    if (offset != 8'h00) begin
                                           offset <= offset - 12'd1;
-                                          start <= {24'b0, offset[7:0]};
+                                          start <= {24'b0, offset[7:0] - 8'd1};
+                                    end
                               end else if (!c0 && !c1 && c2 && !c3) begin
                                     offset <= 12'd0;
                                     // RETURN TO RESET
